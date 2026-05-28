21db57fcbededdb37ed0ae3cf66d16016dee590c
Saurab Gautam <missionprojects250@gmail.com>
2026-05-28 14:45:05 +0545

    12-frontend setup

---
+++ frontend/src/components/Layout.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Navbar from './Navbar'
+import Footer from './Footer'
+
+const Layout = ({ children }) => {
+    return (
+        <div className='flex min-h-svh flex-col bg-base-200 text-base-content'>
+            <Navbar />
+
+            <main className='mx-auto w-full max-w-xl flex-1 px-4 py-8 md:px-6 md:py-10'>{children}</main>
+
+            <Footer />
+        </div>
+    )
+}
+
+export default Layout
