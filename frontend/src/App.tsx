53905a837bb1e30aa668dcdb60e66c8b31483bbe
Saurab Gautam <missionprojects250@gmail.com>
2026-05-30 18:42:34 +0545

    14-creating complete homepage

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,4 @@
 // import { SignInButton, SignUpButton, UserButton, useUser } from "@clerk/react";
-
 // function App() {
 //   const { isSignedIn } = useUser();
 
@@ -16,44 +15,65 @@
 //     </div>
 //   );
 // }
+// export default App;
+
+// import { SignInButton, SignUpButton, UserButton, useUser, useAuth } from "@clerk/react";
+// import PageLoader from "./components/PageLoader";
+// import Layout from "./components/Layout";
+
+// const App = () => {
+//   const { isSignedIn } = useUser();
+
+//   const { isLoaded } = useAuth();
+//   if (!isLoaded) return <PageLoader />
+//   return (
+//     <Layout>
+//       <div>
+//         {!isSignedIn ? (
+//           <>
+//             <SignInButton mode="modal" />
+//             <SignUpButton />
+//           </>
+//         ) : (
+//           <UserButton />
+//         )}
+//       </div>
+
+//       <div className="text-7xl">
+//         <button className="btn btn-success">Success</button>
+//         App
+//         <button className="btn btn-primary">Click Here</button>
+//       </div>
+//       <button className="btn btn-secondary">Click Here</button>
+//       <button className="btn btn-xs sm:btn-sm md:btn-md lg:btn-lg xl:btn-xl">
+//         Responsive
+//       </button>
+//       <button className="btn btn-error">Error</button>
+//     </Layout>
+//   );
+// };
 
 // export default App;
-import { useAuth } from "@clerk/react";
-// import React from "react";
-import { SignInButton, SignUpButton, UserButton, useUser } from "@clerk/react";
+
+import { SignInButton, SignUpButton, UserButton, useUser, useAuth } from "@clerk/react";
 import PageLoader from "./components/PageLoader";
 import Layout from "./components/Layout";
+import { Routes, Route } from "react-router";
+import HomePage from "./pages/HomePage";
 
 const App = () => {
-  const { isSignedIn } = useUser();
 
   const { isLoaded } = useAuth();
-  if (!isLoaded) return <PageLoader />
+
   return (
     <Layout>
-      <div>
-        {!isSignedIn ? (
-          <>
-            <SignInButton mode="modal" />
-            <SignUpButton />
-          </>
-        ) : (
-          <UserButton />
-        )}
-      </div>
 
-      <div className="text-7xl">
-        <button className="btn btn-success">Success</button>
-        App
-        <button className="btn btn-primary">Click Here</button>
-      </div>
-      <button className="btn btn-secondary">Click Here</button>
-      <button className="btn btn-xs sm:btn-sm md:btn-md lg:btn-lg xl:btn-xl">
-        Responsive
-      </button>
-      <button className="btn btn-error">Error</button>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+
     </Layout>
-  );
-};
+  )
+}
 
-export default App;
+export default App
