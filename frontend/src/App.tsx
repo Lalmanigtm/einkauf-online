98637f183105e77bbcf9c1df4b70a0cabb0b58cf
Saurab Gautam <missionprojects250@gmail.com>
2026-06-01 17:32:21 +0545

    20-admin page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -11,6 +11,7 @@ import OrderDetailPage from "./pages/OrderDetailPage";
 import OrderSummaryPage from "./pages/OrderSummaryPage";
 import OrderChatPage from "./pages/OrderChatPage";
 import OrderVideoPage from "./pages/OrderVideoPage";
+import AdminProductsPage from "./pages/AdminProductsPage";
 
 const App = () => {
   const { isLoaded, isSignedIn } = useAuth();
@@ -28,7 +29,18 @@ const App = () => {
         <Route path="/checkout/return" element={<CheckoutReturnPage />} />
 
         <Route path="/demo-sentry" element={<SentryDemoPage />} />
-        <Route path="/orders/:id/call" element={isSignedIn ? <OrderVideoPage /> : <Navigate to={"/"} replace />} />
+        <Route
+          path="/orders/:id/call"
+          element={
+            isSignedIn ? <OrderVideoPage /> : <Navigate to={"/"} replace />
+          }
+        />
+        <Route
+          path="/admin"
+          element={
+            isSignedIn ? <AdminProductsPage /> : <Navigate to="/" replace />
+          }
+        />
         <Route path="/orders/:id" element={<OrderDetailPage />}>
           <Route index element={<OrderSummaryPage />} />
           <Route path="chat" element={<OrderChatPage />} />
@@ -39,4 +51,3 @@ const App = () => {
 };
 
 export default App;
-
